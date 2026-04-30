411442bffce4b4e5186b69a76d9bd3bc0faa9b94
paskoyura7-netizen <paskoyura7@gmail.com>
2026-04-30 17:08:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const BOT_ID     = '7634463423774031877';
 const API_BASE   = 'https://api.coze.com';
 const texts = [
   "Привіт 👋",
-  "Я твій AI друг!",
+  "Я твій AI друг",
   "Створений для допомоги студентам ІФНТУНГ"
 ];
 
@@ -52,56 +52,7 @@ function showChat() {
 
 
 
-const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
 
-recognition.lang = 'uk-UA';
-recognition.interimResults = false;
-
-function startVoiceInput() {
-  recognition.start();
-}
-
-
-
-recognition.onresult = function(event) {
-  const text = event.results[0][0].transcript;
-
-  messageInput.value = text;
-};
-
-async function sendMessage() {
-  const message = messageInput.value.trim();
-  if (!message) return;
-
-  addMessage(message, "user");
-  messageInput.value = "";
-
-  const typingEl = addTypingIndicator();
-
-  try {
-    const reply = await callCozeAPI(message);
-    typingEl.remove();
-
-    addMessage(reply, "ai");
-    speak(reply); 
-  } catch (error) {
-    typingEl.remove();
-    addMessage("Помилка", "ai");
-  }
-}
-recognition.onerror = function(event) {
-  console.error("Speech error:", event.error);
-  alert("Помилка мікрофона: " + event.error);
-};
-
-function speak(text) {
-  const speech = new SpeechSynthesisUtterance(text);
-  speech.lang = "uk-UA";
-  window.speechSynthesis.speak(speech);
-}
-if (!('SpeechRecognition' in window || 'webkitSpeechRecognition' in window)) {
-  console.warn("SpeechRecognition не підтримується");
-}
 
 
 
@@ -316,3 +267,53 @@ sendBtn.addEventListener("click", sendMessage);
 
 
 
+const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+
+recognition.lang = 'uk-UA';
+recognition.interimResults = false;
+
+function startVoiceInput() {
+  recognition.start();
+}
+
+
+
+recognition.onresult = function(event) {
+  const text = event.results[0][0].transcript;
+
+  messageInput.value = text;
+};
+
+async function sendMessage() {
+  const message = messageInput.value.trim();
+  if (!message) return;
+
+  addMessage(message, "user");
+  messageInput.value = "";
+
+  const typingEl = addTypingIndicator();
+
+  try {
+    const reply = await callCozeAPI(message);
+    typingEl.remove();
+
+    addMessage(reply, "ai");
+    speak(reply); 
+  } catch (error) {
+    typingEl.remove();
+    addMessage("Помилка", "ai");
+  }
+}
+recognition.onerror = function(event) {
+  console.error("Speech error:", event.error);
+  alert("Помилка мікрофона: " + event.error);
+};
+
+function speak(text) {
+  const speech = new SpeechSynthesisUtterance(text);
+  speech.lang = "uk-UA";
+  window.speechSynthesis.speak(speech);
+}
+if (!('SpeechRecognition' in window || 'webkitSpeechRecognition' in window)) {
+  console.warn("SpeechRecognition не підтримується");
+}
