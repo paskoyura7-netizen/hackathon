07a919c5fe731ebeb5f3f1e8d87319ca55c6a88e
paskoyura7-netizen <paskoyura7@gmail.com>
2026-04-30 16:55:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -244,3 +244,58 @@ messageInput.addEventListener("keydown", (e) => {
 });
 
 sendBtn.addEventListener("click", sendMessage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+
+recognition.lang = 'uk-UA';
+recognition.interimResults = false;
+
+function startVoiceInput() {
+  recognition.start();
+}
+
+
+
+recognition.onresult = function(event) {
+  const text = event.results[0][0].transcript;
+
+  messageInput.value = text;
+  sendMessage();
+};
+
+async function sendMessage() {
+  const message = messageInput.value.trim();
+  if (!message) return;
+
+  addMessage(message, "user");
+  messageInput.value = "";
+
+  const typingEl = addTypingIndicator();
+
+  try {
+    const reply = await callCozeAPI(message);
+    typingEl.remove();
+
+    addMessage(reply, "ai");
+    speak(reply); 
+  } catch (error) {
+    typingEl.remove();
+    addMessage("Помилка", "ai");
+  }
+}
+recognition.onerror = function(event) {
+  console.error("Speech error:", event.error);
+  alert("Помилка мікрофона: " + event.error);
+};
